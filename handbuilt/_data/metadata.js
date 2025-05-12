4552e655621dae8b48fea94c8ca67c330a25a710
Sophia Dengo <sophiadengo@Sophias-MacBook-Pro.local>
2025-05-12 11:48:56 -0500

    v0

---
+++ handbuilt/_data/metadata.js
@@ -0,0 +1,12 @@
+export default {
+  title: "Martin Asturias, Director of Photography",
+  url: "http://gmaprotv.com",
+  language: "en",
+  description:
+    "Martin Asturias is a veteran director of photography, specializing in journalism and documentary film.",
+  author: {
+    name: "Sophia Dengo",
+    email: "sdengo@proton.me",
+    url: "https://sophiadengo.com",
+  },
+};
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+// Initialize USWDS components
+window.addEventListener("load", function () {
+  if (typeof window.uswdsInit === "function") {
+    window.uswdsInit();
+  }
+});
